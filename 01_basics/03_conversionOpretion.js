5586ac409ec86e20fff818065819c183319c76de
Harsh Solanki <harshsolanki2899@gmail.com>
2025-06-17 02:48:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOpretion.js
@@ -0,0 +1,27 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueinNumber = Number(score)
+// console.log(typeof valueinNumber);
+// console.log( valueinNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1 ; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false  
+// "harsh" => true
+
+let oneNumber = 33
+
+let stringNumber = String(oneNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
