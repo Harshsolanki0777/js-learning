b1496996cb65284c5e9aa5548e2f9221603d22d0
Harsh Solanki <harshsolanki2899@gmail.com>
2025-06-21 04:15:58 +0000

    scope levels and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,18 +1,56 @@
+
+
 // var c = 300     // Globle scope
 let a = 300
 
 if (true) {
     let a = 10
     const b = 20
-    console.log("Inner :", a);
+    // console.log("Inner :", a);
     
 }//block scope
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "harsh"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+// one()
+
+if (true) {
+    const username = "harsh"
+    if (username === "harsh") {
+        const website = " youtube"
+        // console.log(username + website);
+    } 
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++++++++++ interesting ++++++++++++++++++++++++++++
+
+
+
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1  
+}
+
+const addTwo = function(num){
+    return num + 2
+} 
+addTwo(5)
