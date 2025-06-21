991c32f695aa8c678c156e8ea90e598cabb51b6f
Harsh Solanki <harshsolanki2899@gmail.com>
2025-06-21 03:29:29 +0000

    function with array & objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,34 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("harsh"));
-console.log(loginUserMessage("harsh"));
+// console.log(loginUserMessage("harsh"));
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(300, 300, 400, 4000));
+
+const user = {
+    username : "harsh",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} price is ${anyobject.price}`);   
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 299
+})
+
+const myNewArray = [200, 300, 500, 800]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 400, 500]));
